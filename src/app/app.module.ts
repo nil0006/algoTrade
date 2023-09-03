ab3eecffbcef14a75fcedbb7a681d4e18d2856e6
nil0006 <niladrikarmakara006@gmail.com>
2023-09-03 11:48:00 +0530

    component add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CourseComponent } from './components/course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    SignupComponent,
+    HomeComponent,
+    FooterComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
