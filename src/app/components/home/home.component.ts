8641200d4ae770ab492e39620771a88b4b9a2e8f
nil0006 <niladrikarmakara006@gmail.com>
2023-09-09 19:37:54 +0530

    uichanged

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  constructor(public router:Router){}
+  routeToAbout(){
+    this.router.navigate([''],{fragment: 'about'})
+  }
 }
